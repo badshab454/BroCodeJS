1806246cc4393b2e3675885948c9ba312309c17f
Badsha Hossain <badshab454@gmail.com>
2024-08-11 16:32:04 +0600

    arithmetic operator

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var name = "Badsha Hossain";
+var age = 27;
+var isMarried = false;
+
+console.log(`${name} is ${age} years old.`);
+
+let students = 30;
+
+students = students + 5;
+
+console.log(students);
+
